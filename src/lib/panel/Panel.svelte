717afe08bb4bf2b15de2c26b5270b6abeea8c49f
GHOST <ghostdevbusiness@gmail.com>
2025-02-15 16:06:46 +0000

    feat: add pane system

---
+++ src/lib/panel/Panel.svelte
@@ -0,0 +1,13 @@
+<div class="panel">
+	<p>hello world</p>
+</div>
+
+<style>
+	.panel {
+		background-color: var(--background-secondary);
+		border-radius: 12px;
+		height: 100%;
+		padding: 22px;
+		min-width: 240px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,87 @@
 <script lang="ts">
+	import IconGripHorizontal from 'lucide-svelte/icons/grip-horizontal';
+	import IconGripVertical from 'lucide-svelte/icons/grip-vertical';
+	import { Pane, PaneGroup, PaneResizer } from 'paneforge';
+	import Panel from '$lib/panel/Panel.svelte';
 	import Map from '$lib/map/Map.svelte';
+
+	let isDragging = $state(false);
+	let innerWidth = $state(0);
+
+	type Direction = 'vertical' | 'horizontal';
+
+	let direction: Direction = $derived(
+		innerWidth <= 830 ? 'vertical' : 'horizontal',
+	);
+
+	let panelMin = $derived(Math.ceil((284 / innerWidth) * 100));
 </script>
 
-<Map />
+<svelte:window bind:innerWidth />
+
+<PaneGroup {direction} class={direction == 'vertical' ? 'pane-reverse' : ''}>
+	<Pane
+		defaultSize={direction == 'horizontal' ? panelMin : 33}
+		minSize={direction == 'horizontal' ? panelMin : 33}
+		order={direction == 'horizontal' ? 1 : 2}>
+		<Panel />
+	</Pane>
+
+	<PaneResizer
+		class="resizer"
+		onDraggingChange={(v: boolean) => {
+			isDragging = v;
+		}}>
+		<div class="resizer-grip {direction}" class:active={isDragging}>
+			{#if direction == 'horizontal'}
+				<IconGripVertical size={18} />
+			{:else}
+				<IconGripHorizontal size={18} />
+			{/if}
+		</div>
+	</PaneResizer>
+
+	<Pane minSize={33} order={direction == 'horizontal' ? 2 : 1}>
+		<Map />
+	</Pane>
+</PaneGroup>
+
+<style>
+	:global(.pane-reverse) {
+		flex-direction: column-reverse !important;
+	}
+
+	:global(.resizer) {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.resizer-grip {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		background-color: var(--background-tertiary);
+		border-radius: 4px;
+		z-index: 1000;
+
+		transition: background-color 0.2s ease-in-out;
+
+		&.horizontal {
+			padding: 6px 2px;
+			margin: 0px -4px;
+		}
+
+		&.vertical {
+			padding: 2px 6px;
+			margin: -4px 0px;
+		}
+
+		&:hover,
+		&:active,
+		&.active {
+			background-color: var(--primary);
+		}
+	}
+</style>
