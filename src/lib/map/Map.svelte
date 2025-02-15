07945d54a7bf2cc4c411cc68d01a08863e87af2a
GHOST <ghostdevbusiness@gmail.com>
2025-02-15 20:22:06 +0000

    feat: add ability to change map theme

--- src/lib/map/Map.svelte
+++ src/lib/map/Map.svelte
@@ -3,6 +3,8 @@
 	import IconTrack from 'lucide-svelte/icons/train-track';
 	import IconAntenna from 'lucide-svelte/icons/antenna';
 	import IconGauge from 'lucide-svelte/icons/gauge';
+	import IconMoon from 'lucide-svelte/icons/moon';
+	import IconSun from 'lucide-svelte/icons/sun';
 	import { ctx } from '$lib/state.svelte';
 	import { onMount, tick } from 'svelte';
 	import {
@@ -17,8 +19,15 @@
 		AttributionControl,
 	} from 'svelte-maplibre-gl';
 
-	const STYLES = ['standard', 'signals', 'maxspeed'];
-	let style = $state('standard');
+	const RAIL_STYLES = ['standard', 'signals', 'maxspeed'];
+
+	let mapTheme = $state<'light' | 'dark'>('light');
+	let railStyle = $state('standard');
+
+	function nextStyle() {
+		const index = RAIL_STYLES.indexOf(railStyle);
+		railStyle = RAIL_STYLES[(index + 1) % RAIL_STYLES.length];
+	}
 
 	let mainControlPosition: maplibregl.ControlPosition = $derived(
 		ctx.direction == 'horizontal' ? 'top-left' : 'bottom-right',
@@ -39,7 +48,9 @@
 <MapLibre
 	bind:map
 	class="map"
-	style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+	style={mapTheme === 'light'
+		? 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json'
+		: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'}
 	zoom={4}
 	center={{ lng: -0.114, lat: 51.503 }}
 	attributionControl={false}>
@@ -49,17 +60,26 @@
 	<GlobeControl position={mainControlPosition} />
 	<GeolocateControl position={mainControlPosition} />
 	<CustomControl position={mainControlPosition}>
-		<button
-			title="Map Style: {style}"
-			onclick={() => {
-				style = STYLES[(STYLES.indexOf(style) + 1) % STYLES.length];
-			}}>
-			{#if style === 'standard'}
+		<button title="Map Style: {railStyle}" onclick={nextStyle}>
+			{#if railStyle === 'standard'}
 				<IconTrack color="#333" size="22" />
-			{:else if style === 'signals'}
+			{:else if railStyle === 'signals'}
 				<IconAntenna color="#333" size="22" />
-			{:else if style === 'maxspeed'}
+			{:else if railStyle === 'maxspeed'}
 				<IconGauge color="#333" size="22" />
+			{/if}
+		</button>
+	</CustomControl>
+	<CustomControl position={mainControlPosition}>
+		<button
+			title="Change theme to {mapTheme === 'light' ? 'dark' : 'light'}"
+			onclick={() => {
+				mapTheme = mapTheme === 'light' ? 'dark' : 'light';
+			}}>
+			{#if mapTheme === 'light'}
+				<IconMoon color="#333" size="22" />
+			{:else}
+				<IconSun color="#333" size="22" />
 			{/if}
 		</button>
 	</CustomControl>
@@ -68,9 +88,9 @@
 		id="railway"
 		attribution={`<a href="http://www.openrailwaymap.org/">OpenRailwayMap</a> (<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a>)`}
 		tiles={[
-			`https://a.tiles.openrailwaymap.org/${style}/{z}/{x}/{y}.png`,
-			`https://b.tiles.openrailwaymap.org/${style}/{z}/{x}/{y}.png`,
-			`https://c.tiles.openrailwaymap.org/${style}/{z}/{x}/{y}.png`,
+			`https://a.tiles.openrailwaymap.org/${railStyle}/{z}/{x}/{y}.png`,
+			`https://b.tiles.openrailwaymap.org/${railStyle}/{z}/{x}/{y}.png`,
+			`https://c.tiles.openrailwaymap.org/${railStyle}/{z}/{x}/{y}.png`,
 		]}
 		tileSize={256}>
 		<RasterLayer id="railway-layer" source="railway" />
