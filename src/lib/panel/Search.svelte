a3e8d27d0eed8ddaa2b03ea078ba418dfe0b8212
GHOST <ghostdevbusiness@gmail.com>
2025-02-18 01:51:27 +0000

    feat: make map locate it's own button

--- src/lib/panel/Search.svelte
+++ src/lib/panel/Search.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import IconLocate from 'lucide-svelte/icons/map-pin';
 	import { search } from './search.svelte';
 	import { ctx } from '$lib/state.svelte';
 	import { Debounced } from 'runed';
@@ -26,17 +27,25 @@
 		<p>Searching...</p>
 	{:then}
 		{#each search.results as station}
-			<button
-				class="icon result"
-				onclick={() => {
-					ctx.map?.flyTo({
-						center: [station.latitude, station.longitude],
-						zoom: 15,
-					});
-				}}>
-				<strong>{station.name}</strong>
-				<small>({station.railway?.replaceAll('_', ' ')})</small>
-			</button>
+			<details class="result">
+				<summary>
+					<strong>{station.name}</strong>
+					<small>({station.railway?.replaceAll('_', ' ')})</small>
+				</summary>
+
+				<div class="controls">
+					<button
+						class="icon"
+						onclick={() => {
+							ctx.map?.flyTo({
+								center: [station.latitude, station.longitude],
+								zoom: 15,
+							});
+						}}>
+						<IconLocate size={18} />
+					</button>
+				</div>
+			</details>
 		{/each}
 	{:catch error}
 		<p><small>{error}</small></p>
@@ -54,8 +63,21 @@
 		flex-grow: 1;
 
 		.result {
-			text-align: left;
-			text-transform: capitalize;
+			summary {
+				text-align: left;
+				text-transform: capitalize;
+				transition: color 0.2s ease-in-out;
+				cursor: pointer;
+			}
+
+			summary:hover,
+			&:focus-visible summary {
+				color: var(--primary);
+			}
+
+			.controls {
+				padding: 4px 12px;
+			}
 		}
 	}
 </style>
